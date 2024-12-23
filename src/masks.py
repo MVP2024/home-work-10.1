baee048c895604451b461475ef0bd6585e59a0aa
Valery Medvedev <walery.medvedev@gmail.com>
2024-12-23 14:05:25 +0300

    модификация функций mask_account_card
     get_mask_card_number
     get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -1,24 +1,38 @@
-from typing import Union
+from typing import Union, Any
 
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
-    """Функция принимает на вход номер карты и
+    """Функция принимает на вход номер карты и возвращает замаскированный номер"""
 
-       возвращает замаскированный номер
+    # проверяет если в номере карты не 16 символов, то возвращает ошибку.
+    if len(card_number) != 16:
+        return "Ошибка, некорректный номер карты"
 
-    в формате XXXX XX** **** XXXX
-    """
+    # если в номере карты присутствуют кроме цифр, другие символы, то возвращает ошибку.
+    for char in card_number:
+        if not char.isdigit():
+            return "Ошибка, проверьте правильность ввода номера карты"
 
-    masked_number = card_number[:4] + " " + card_number[6:8] + "** **** " + card_number[-4:]
-    return masked_number
+    # маскирует номер карты.
+    masked_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
+
+    # возвращаем замаскированный номер в формате XXXX XX** **** XXXX.
+    return masked_card_number
 
 
-def get_mask_account(account_number: Union[str]) -> Union[str]:
-    """Функция принимает на вход номер счёта и
+def get_mask_account(account_number: Union[str]) -> str | ValueError | Any:
+    """Функция принимает на вход номер счёта и возвращает замаскированный счёт"""
+    try:
 
-    возвращает замаскированный счёт в формате
+        # проверяем если в номере счёта присутствуют вместо цифр другие символы
+        # и длинна цифр не равна 16, то возвращает ошибку.
+        if not account_number.isdigit() or len(account_number) != 16:
+            raise ValueError("Ошибка: неправильный номер счёта")
 
-                **XXXX
-    """
-    masked_number = "**" + account_number[-4:]
-    return masked_number
+        # маскируем номер счёта
+        masked_account = "**" + account_number[-4:]
+
+        # возвращаем замаскированный номер.
+        return masked_account
+    except ValueError as ve:
+        return ve
--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,62 @@
-from typing import Union
+from typing import Union, Any
 
 
-def mask_account_card(types_requisites: Union[str]) -> str:
-    """Функция принимает на вход вид и номер карты или счёт с номером"""
+def mask_account_card(types_requisites: Union[str]) -> str | ValueError | Any:
+    try:
+        """Функция принимает на вход вид и номер карты или счёта с номером"""
 
-    if "Счёт" in types_requisites or "Счет" in types_requisites:
+        # создаём переменные-списки для букв и цифр из аргумента.
+        card_type = []
+        card_number = []
 
-        """Сравнивает 'счёт' или 'счет' в списке и возвращает замаскированный список"""
-        """если указан счёт, возвращает <type> <**XXXX>"""
+        # если есть буквы и пробелы, то добавляем в card_type и переводим в нижний регистр.
+        for char in types_requisites:
+            if char.isalpha() or char.isspace():
+                card_type.append(char.lower())
 
-        return types_requisites[:4] + " " + "**" + types_requisites[-4:]
+            # если есть цифры, то добавляем в card_number.
+            elif char.isdigit():
+                card_number.append(char)
 
-    elif "Visa" in types_requisites or "MasterCard" in types_requisites or "Maestro" in types_requisites:
-        """Сравнивает 'Visa' или 'MasterCard' или 'Maestro' в списке и возвращает
+            # если элементы не цифры, то возникает ошибка.
+            elif not char.isdigit():
+                raise ValueError("Ошибка: Присутствуют другие символы в номере карты.")
+            else:
+                raise ValueError("Ошибка: проверьте правильность номера карты")
 
-        имя карты и её замаскированный номер"""
+        # если номер карты или счёта больше 16 символов, то возникнет ошибка.
+        if len(card_number) > 16:
+            raise ValueError("Ошибка: Слишком длинный номер карты")
 
-        """возвращает название и замаскированный номер, если указана карта
+        # если номер карты меньше 16 символов, то возникнет ошибка.
+        elif len(card_number) < 16:
+            raise ValueError("Ошибка: Номер карты короткий")
 
-        в формате < type > < XXXX XX ** ** ** XXXX >"""
+        card_type_str = "".join(card_type)
+        card_number_str = "".join(card_number)
 
-        return types_requisites[0:-12] + " " + "** **** " + types_requisites[-4:]
+        # сверяет, есть ли слова "счет" или "счёт" в объединённой строке "card_type_str".
+        if "счет" in card_type_str or "счёт" in card_type_str:
+
+            # возвращаем копию строки со словами с заглавными буквами методом "capitalize()".
+            card_type_str = card_type_str.capitalize()
+
+            # маска номера счёта.
+            masked_card_number = "Счет **" + card_number_str[-4:]
+
+        # с помощью функции "any()" ищем определённый объект в итерируемой строке "card_type_str".
+        elif any(word in card_type_str for word in ["visa", "maestro", "mastercard", "union"]):
+
+            # объединяем слова с заглавными буквами и разделяем методом "split()".
+            card_type_str = " ".join(word.capitalize() for word in card_type_str.split())
+
+            # маскируем номер карты.
+            masked_card_number = card_type_str + " " + card_number_str[:-12] + " " + "** **** " + card_number_str[-4:]
+
+        # возвращает название и замаскированный номер карты или счёта.
+        return masked_card_number
+    except ValueError as ve:
+        return ve
 
 
 def get_date(data_full: Union[str]) -> str:
