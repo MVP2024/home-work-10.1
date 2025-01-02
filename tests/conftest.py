9509b5c40f62d5b2131bfd22cfe8029240391f27
Valery Medvedev <walery.medvedev@gmail.com>
2025-01-02 15:43:34 +0300

    фикстуры для filter_by_currecy

--- tests/conftest.py
+++ tests/conftest.py
@@ -162,49 +162,7 @@ def invalid_characters():
 @pytest.fixture
 def transactions():
     return [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 123456789,
-            "state": "EXECUTED",
-            "date": "2019-05-01T12:00:00.000000",
-            "operationAmount": {
-                "amount": "500.00",
-                "currency": {
-                    "name": "EUR",
-                    "code": "EUR"
-                }
-            },
-            "description": "Перевод в евро",
-            "from": "Счет 11111111111111111111",
-            "to": "Счет 22222222222222222222"
-        }
+        {"operationAmount": {"amount": "100", "currency": {"code": "USD"}}},
+        {"operationAmount": {"amount": "200", "currency": {"code": "EUR"}}},
+        {"operationAmount": {"amount": "300", "currency": {"code": "USD"}}},
     ]
