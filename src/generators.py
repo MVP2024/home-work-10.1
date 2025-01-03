dd0cd62a7bd5d89075a292f74093efdcf7be28d6
Valery Medvedev <walery.medvedev@gmail.com>
2025-01-03 12:53:56 +0300

    Модификация функции filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,29 +1,15 @@
-from typing import List, Dict, Any, Iterator
+from typing import List, Dict, Iterator
 
-
-def filter_by_currency(transactions, currency):
+def filter_by_currency(transactions: List[Dict[str, Dict[str, str]]], currency: str) -> Iterator[Dict[str, Dict[str, str]]]:
     """
-    Функция фильтрует транзакции по заданной валюте и возвращает итератор с соответствующими транзакциями.
+    Функция возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной.
 
-    :param transactions: Список словарей, представляющих транзакции.
-    :param currency_code: Код валюты, по которой необходимо фильтровать транзакции.
-    :return: Итератор с транзакциями, соответствующими заданной валюте.
-    :raises ValueError: Если transactions не является списком или currency_code не является строкой.
-    :raises ValueError: Если transactions пустой или не содержит операций с указанной валютой.
+    :param transactions: Список словарей с транзакциями.
+    :param currency: Валюта, по которой нужно фильтровать транзакции.
+    :return: Итератор, выдающий транзакции заданной валюты.
     """
-    if not isinstance(transactions, list):
-        raise ValueError("transactions должен быть списком.")
-    if not isinstance(currency, str):
-        raise ValueError("currency_code должен быть строкой.")
-    if not transactions:
-        raise ValueError("Список transactions не должен быть пустым.")
-
-    filtered_transactions = (
-        transaction for transaction in transactions
-        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
-    )
-
-    if not any(filtered_transactions):
-        raise ValueError(f"Нет транзакций с валютой: {currency}")
-
-    return filtered_transactions
+    for transaction in transactions:
+        # Проверяем, соответствует ли валюта
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+            yield transaction
