7a13df58018afe9b6474f2c345d5dc559fba9059
Valery Medvedev <walery.medvedev@gmail.com>
2025-01-19 21:32:12 +0300

    Добавлено логгирование функции load_transactions

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
 import json
 from typing import Any, Dict, List
+import logging
+from src.logger import setup_logger
 
 
+# Настройка логгера для модуля utils
+logger = setup_logger('utils')
 def load_transactions(file_path: str) -> List[Dict[str, Any]]:
     """
     Загружает данные о транзакциях из JSON-файла.
@@ -9,15 +13,19 @@ def load_transactions(file_path: str) -> List[Dict[str, Any]]:
     :param file_path: Путь до JSON-файла.
     :return: Список словарей с данными о транзакциях или пустой список.
     """
+    logger.debug(f"Попытка загрузить транзакции из файла: {file_path}")  # Логируем попытку загрузки
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if isinstance(data, list):
+                logger.info(f"Успешно загружено {len(data)} транзакций.")  # Логируем успешную загрузку
+
                 return data
+            logger.warning("Загруженные данные не являются списком. Возвращается пустой список.")
             return []
     except FileNotFoundError:
-        print(f"Файл не найден: {file_path}")
+        logger.error(f"Файл не найден: {file_path}")  # Логируем ошибку
         return []
     except (json.JSONDecodeError, ValueError) as e:
-        print(f"Ошибка при загрузке JSON: {e}")  # Сообщение для проверки.
+        logger.error(f"Ошибка при загрузке JSON: {e}")  # Логируем ошибку
         return []
