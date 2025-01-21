64674f461f9dd9c5f29e97546d9c85b9c34e04c8
Valery Medvedev <walery.medvedev@gmail.com>
2025-01-21 21:49:39 +0300

    добавлена функция для считывания финансовых операций из файла xlsx

--- src/financial_transactions.py
+++ src/financial_transactions.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from typing import Dict
 
 from src.main import transaction
@@ -18,3 +19,13 @@ def read_financial_operations_from_csv(file_path: str) -> list[Dict[str, str]]:
 			transaction.append(row)
 	return transactions_from_csv
 
+
+def read_financial_operations_from_excel(file_path: str) -> list[Dict[str, str]]:
+	"""
+    Считывает финансовые операции из файла xlsx
+    :param file_path:
+    :return: Список словарей с транзакциями.
+    """
+	df = pd.read_excel(file_path)
+	transactions_from_xlsx = df.to_dict(orient='records')
+	return transactions_from_xlsx
