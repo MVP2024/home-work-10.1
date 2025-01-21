fe8268bea737d25d87055a692c98ff0dfe57fd5b
Valery Medvedev <walery.medvedev@gmail.com>
2025-01-21 20:27:17 +0300

    коррекция декоратора, чтобы записывал всё, а не последнюю вызванную функцию.

--- src/decorators.py
+++ src/decorators.py
@@ -15,7 +15,7 @@ def log(filename: Optional[str] = None, time_delay: float = 0) -> Callable:
     def write_log(information: str) -> None:
         """Записывает информацию в файл или выводит в консоль."""
         if filename:
-            with open(filename, "w", encoding="utf-8") as f:
+            with open(filename, "a", encoding="utf-8") as f:
                 f.write(information + "\n")
         else:
             print(information)
@@ -37,7 +37,7 @@ def log(filename: Optional[str] = None, time_delay: float = 0) -> Callable:
                 write_log(
                     f"{function.__name__} ok. " f"Возвращаемое значение {result}. Время выполнения: {execution_time}",
                 )
-                return result  # Возвращаем резкльтат функции
+                return result  # Возвращаем результат функции
             except Exception as e:
                 write_log(
                     f"{function.__name__}) error: {type(e).__name__}. " f"Inputs: {args}, {kwargs}. Ошибка: {str(e)}",
--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ import os
 
 def setup_logger(module_name: str) -> logging.Logger:
     # Определяем абсолютный путь к корню проекта
-    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ))
     logs_dir = os.path.join(project_root, "logs")
 
     # Создаем папку logs, если она не существует
@@ -17,9 +17,9 @@ def setup_logger(module_name: str) -> logging.Logger:
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)  # Уровень логирования
 
-    # Создаем обработчик для записи логов в файл
+    # Создаем обработчик для записи логов в файл с указанием кодировки и режима 'w'
     log_file_path = os.path.join(logs_dir, f"{module_name}.log")
-    file_handler = logging.FileHandler(log_file_path)
+    file_handler = logging.FileHandler(log_file_path, mode='w', encoding='utf-8')  # Указываем кодировку и режим
     file_handler.setFormatter(formatter)
 
     # Добавляем обработчик к логгеру
